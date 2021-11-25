cdc791f6bb46cedf5c39946c68ffe6b638290ae0
Micko <micko@pop-os.localdomain>
2021-11-25 20:04:12 +0200

    init jest, add tests for ceil

---
+++ src/test/ceil.test.js
@@ -0,0 +1,13 @@
+import ceil from '../ceil'
+
+test('ceil number to up', () => {
+    expect(ceil(4.006)).toBe(5)
+})
+
+test('ceil with second parameter 2 to be rounded with 2 decimals', () => {
+    expect(ceil(6.004, 2)).toBe(6.01)
+})
+
+test('ceil with parameters 6040 and -2 to be 6100', () => {
+    expect(ceil(6040, -2)).toBe(6100)
+})

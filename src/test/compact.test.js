ecdcc0aa240f2f12e858dce0770e07fa2a240de5
Micko <micko@pop-os.localdomain>
2021-11-25 20:58:24 +0200

    add compact.test.js, function now working correctyl

---
+++ src/test/compact.test.js
@@ -0,0 +1,15 @@
+import compact from "../compact";
+
+// The compact functon return very wierd array here
+test.skip('remove falsey values from array', () => {
+    const array = [0, 1, false, 2, '', 3]
+    const filteredArray = compact(array)
+
+    const res = [1, 2, 3]
+    console.log(res) //  [ 1, 2, 3 ]
+
+    console.log(filteredArray) //  [ 2, 3, '-1': 1 ]
+
+
+    expect(filteredArray).toEqual(res);
+})
